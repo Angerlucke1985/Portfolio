bd4166d81aab723e4ad26da3e3d53cffb8f9242b
Angerlucke1985 <tomicaprini@outlook.es>
2024-03-30 23:26:19 -0300

    ddsa

--- Js/sweetAlert.js
+++ Js/sweetAlert.js
@@ -1,15 +1,62 @@
 Swal.fire({
 
-	title: '<img class="gif" src="img del principio/5ln4.gif">',
-   width: '70%',
+   title: '<img class="gif" src="img del principio/5ln4.gif">',
+  // text:
+  // html:
+  // icon: 'error'
+  // confirmButtonText:
+  // footer:
+  width: '70%',
+  // padding:
+  background: '#ECE97F',
+  // grow:
+  // backdrop:
+  // timer:
+  // timerProgressBar:
+  // toast:
+  // position:
+   allowOutsideClick: false,
+  allowEscapeKey: false,
+  // allowEnterKey:
+  stopKeydownPropagation: true,
 
-   background: '#ECE97F',
+  // input:
+  // inputPlaceholder:
+  // inputValue:
+  // inputOptions:
+  
+  //  customClass:
+  // 	container:
+  // 	popup:
+  // 	header:
+  // 	title:
+  // 	closeButton:
+  // 	icon:
+  // 	image:
+  // 	content:
+  // 	input:
+  // 	actions:
+  // confirmButton: '200px',
+  // 	cancelButton:
+  // 	footer:	
 
-	allowOutsideClick: false,
-   allowEscapeKey: false,
+  // showConfirmButton:
+  // confirmButtonColor:
+  // confirmButtonAriaLabel:
 
-   stopKeydownPropagation: true,
-   
-   showCloseButton: true,
+  // showCancelButton:
+  // cancelButtonText:
+  // cancelButtonColor:
+  // cancelButtonAriaLabel:
+  
+  // buttonsStyling:
+  showCloseButton: true,
+  // closeButtonAriaLabel:
+
+
+  // imageUrl:
+  // imageWidth:
+  // imageHeight:
+  // imageAlt:
+
 })
-

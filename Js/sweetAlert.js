01d6c899fbd4ac9f1ca19d27fe81da7deeaede82
Angerlucke1985 <tomicaprini@outlook.es>
2024-03-30 22:30:00 -0300

    mi pagina

---
+++ Js/sweetAlert.js
@@ -0,0 +1,15 @@
+Swal.fire({
+
+	title: '<img class="gif" src="img del principio/5ln4.gif">',
+   width: '70%',
+
+   background: '#ECE97F',
+
+	allowOutsideClick: false,
+   allowEscapeKey: false,
+
+   stopKeydownPropagation: true,
+   
+   showCloseButton: true,
+})
+
